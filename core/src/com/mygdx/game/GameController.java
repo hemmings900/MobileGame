c7bf0ae3bf2423be4c81bcd355999f0c272637da
hemmings900 <hemmings900@gmail.com>
2014-12-15 23:59:23 +1300

    Jumping Fixed
    Got jumping working in Game update method. Instead of working off time
    duration, the jump is timed by how many updates its duration spans.

--- core/src/com/mygdx/game/GameController.java
+++ core/src/com/mygdx/game/GameController.java
@@ -4,6 +4,7 @@ import java.awt.Point;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.mygdx.gameObjects.GameObject;
 import com.mygdx.gameObjects.PlayerCharacter;
 import com.mygdx.gameObjects.Sprite;
@@ -26,18 +28,14 @@ public class GameController {
 	private Point mousePosition;
 	private BlockController blocks;
 	private CollisionController collisions;
-
+	private int jumpTimer;
+	private int jumpTimeLimit;//Jump time limit is not based on amount of timer updates.
 	
 	/*
 	 * Initializes game settings and values.
 	 */
 	public GameController() throws Exception{
 		mousePosition = new Point(0,0);
-//		jumpTimer = 0;
-//		jumpTimeLimit = 1;
-		//Initialize rendering tools
-//		batch = new SpriteBatch();
-		//shapes = new ShapeRenderer();	
 		
 		//Initialize player
 		Texture img = new Texture("badlogic.jpg");
@@ -45,6 +43,8 @@ public class GameController {
 		Texture img3 = new Texture("playerJump.png");
 		Sprite playerSprite = new Sprite(new Texture[]{img,img2,img3});
 		player = new PlayerCharacter(0,0,20,playerSprite);
+		jumpTimer = 0;
+		jumpTimeLimit = 30;
 		
 		//Initialize Blocks
 		blocks = new BlockController(10);
@@ -55,9 +55,6 @@ public class GameController {
 	 * Game logic handled in here. Called in main render method.
 	 */
 	public void updateGame(){
-		if(Gdx.input.isKeyPressed(Keys.SPACE)){
-			player.setIsJumping(true);
-		}
 		mousePosition.x = Gdx.input.getX();
 		mousePosition.y = ((Gdx.input.getY()-Gdx.graphics.getHeight())*-1);
 		blocks.addRandomBlock(1,100);
@@ -80,19 +77,32 @@ public class GameController {
 					collided = true;
 					}
 				}	
-			}		
-			/*
-			 * If player isn't jumping, check if player is touching any of the blocks.
-			 * Change the sprite image based on state of player.
-			 */
-			if(player.isJumping()){
-				player.getObjectSprite().setCurrentFrame(2);
-			}else if(collided){
-				player.getObjectSprite().setCurrentFrame(1);
 			}
-			else{
-				player.getObjectSprite().setCurrentFrame(0);
+		//Player action processing
+		if(Gdx.input.isButtonPressed(Buttons.LEFT)){
+			player.setIsJumping(true);
+		}
+		/*
+		 * If player isn't jumping, check if player is touching any of the blocks.
+		 * Change the sprite image based on state of player.
+		 */
+		if(player.isJumping()){
+			if(jumpTimer > jumpTimeLimit){
+				player.setIsJumping(false);
+				jumpTimer = 0;
 			}
+			else{			
+			player.getObjectSprite().setCurrentFrame(2);
+			jumpTimer++;
+			}
+		}else if(collided){
+			player.getObjectSprite().setCurrentFrame(1);
+		}
+		else{
+			player.getObjectSprite().setCurrentFrame(0);
+		}
+		
+			
 			player.FollowPoint(mousePosition);
 		}
 	/*###################################################
